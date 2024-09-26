34c955515705a0be71c86046456e13cd2ab966e0
Eric Raguzin <eraguzin@bnl.gov>
2024-09-26 18:25:36 -0400

    Small changes to go from Modelsim to simulation input

--- vcd2sim_cal.py
+++ vcd2sim_cal.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-#Script by Eric to take VCD output from Identify and convert certain signals to a CSV
-#The VCD format from Identify puts vectors on the same line instead of separately bit by bit
-#So the analysis is different here than the ModelSim VCD analyzers.
-#You tell it which signals to write to CSV through the command line
+#Script by Eric to take VCD output from Libero's ModelSim and convert certain signals to a DAT for further simulation
+#You tell it which signals to write to DAT through the command line
 import sys, os, json, subprocess
 from vcd.reader import TokenKind, tokenize
 import numpy as np
@@ -12,6 +10,15 @@ import csv
 import pprint
 
 class LuSEE_Calibration_Test:
+    def __init__(self):
+        print("Python --> Welcome to the LuSEE VCD Converter")
+        self.pp = pprint.PrettyPrinter(indent=4)
+        self.time_print = 0
+        self.time_print_add = 1E12
+        self.time_prefix = "ms"
+        self.start_time = 0
+        self.time_tick = 50000 #One clock cycle is 100 ns in this simulation
+
     def analyze_file(self, name, signals):
         if (not os.path.isfile(f"{name}")):
             sys.exit(f"Python --> {name} does not exist")
@@ -45,43 +52,47 @@ class LuSEE_Calibration_Test:
                 reference = i.var.reference
                 bit_index = i.var.bit_index
                 array_type = type(bit_index)
+                #print(f"{id_code}, {reference}, {bit_index}, {array_type}")
                 #This means it's an array of an array, so like $var wire 1 " pks_ref [0][12] $end
                 #This correction assumes you can only have double nested arrays. It will have to be fixed if you can have triple or higher
                 if array_type is tuple:
                     reference = f"{reference}_{bit_index[0]}"
                     bit_index = bit_index[1]
                 #Want to create signals that keep the arrays together.
-                if reference not in self.pairs:
-                    #First time a signal name is found. A key in the master list is made for it as a 1-bit value
-                    #And a spot in the value tracker array ismade too
-                    key_in = {}
-                    key_in[id_code] = bit_index
-                    key_in['bits'] = 1
-                    self.pairs[reference] = key_in
+                if reference in self.signals_of_interest:
+                    if reference not in self.pairs:
+                        print(f"Found {reference} for the first time")
+                        #First time a signal name is found. A key in the master list is made for it as a 1-bit value
+                        #And a spot in the value tracker array ismade too
+                        key_in = {}
+                        key_in[id_code] = bit_index
+                        key_in['bits'] = 1
+                        self.pairs[reference] = key_in
 
-                    val_in = {}
-                    val_in['x'] = []
-                    val_in['y'] = []
-                    val_in['last_val'] = 0
-                    val_in['modified'] = False
-                    self.vals[reference] = val_in
+                        val_in = {}
+                        val_in['x'] = []
+                        val_in['y'] = []
+                        val_in['last_val'] = 0
+                        val_in['modified'] = False
+                        self.vals[reference] = val_in
 
-                else:
-                    #This means this key is part of an array, the extra bit and indicator is added to the master list
-                    key_in = {}
-                    key_in[id_code]=bit_index
-                    key_in['bits'] = self.pairs[reference]['bits'] + 1
-                    self.pairs[reference].update(key_in)
+                    else:
+                        #print(f"Found {reference} again")
+                        #This means this key is part of an array, the extra bit and indicator is added to the master list
+                        key_in = {}
+                        key_in[id_code]=bit_index
+                        key_in['bits'] = self.pairs[reference]['bits'] + 1
+                        self.pairs[reference].update(key_in)
 
-                new_sensitive_var = {}
-                new_sensitive_var[id_code] = reference
-                self.sensitivity_list.update(new_sensitive_var)
+                    new_sensitive_var = {}
+                    new_sensitive_var[id_code] = reference
+                    self.sensitivity_list.update(new_sensitive_var)
 
             #The last line of the preamble/header
             elif (i.kind is TokenKind.ENDDEFINITIONS):
-                #self.pp.pprint (self.pairs)
-                #self.pp.pprint (self.vals)
-                #self.pp.pprint (self.sensitivity_list)
+                # self.pp.pprint (self.pairs)
+                # self.pp.pprint (self.vals)
+                # self.pp.pprint (self.sensitivity_list)
                 break
 
     def body(self):
@@ -93,7 +104,10 @@ class LuSEE_Calibration_Test:
             if (i.kind is TokenKind.CHANGE_TIME):
                 self.prev_time = self.time
                 self.time = int(i.data)
-                for j in self.pairs:
+                if (self.time > self.time_print):
+                    print(f"At {int(self.time/self.time_print_add)} {self.time_prefix}")
+                    self.time_print += self.time_print_add
+                for j in self.signals_of_interest:
                     if (self.vals[j]['modified'] == True):
                         lv = self.vals[j]['last_val']
                         self.vals[j]['x'].append(self.prev_time)
@@ -146,11 +160,15 @@ class LuSEE_Calibration_Test:
             self.vals[i]['y'].append(self.vals[i]['last_val'])
 
         print("Python --> Done with VCD body")
+        print(f"Final time is {int(self.time/self.time_print_add)} {self.time_prefix}")
     def output(self):
+        print("Outputting files")
         for i in self.vals:
             time_list = self.vals[i]['x']
             val_list = self.vals[i]['y']
             print(f"Scanning {i} to array")
+            print(len(time_list))
+            print(len(val_list))
             #Finding when this signal reaches the start time
             prev_j = 0
             index = None
@@ -167,11 +185,14 @@ class LuSEE_Calibration_Test:
             output = []
             time = self.start_time
             next_event = time_list[index]
-            while (time <= self.end_time):
+            while (time <= self.time):
                 if (next_event == time):
                     output.append(val_list[index])
                     index += 1
-                    next_event = time_list[index]
+                    try:
+                        next_event = time_list[index]
+                    except:
+                        print(f"Next event index was {index} and length is {len(time_list)}")
                 elif (next_event > time):
                     output.append(val_list[index-1])
                 else:
@@ -181,67 +202,6 @@ class LuSEE_Calibration_Test:
 
             with open(f"{i}_proc.dat", "w") as f:
                 f.writelines(x + '\n' for x in output)
-    def plot(self):
-        #Find where fft_ready goes from 0 to 1
-        to_check = self.vals['fft_ready']['y']
-        check = 0
-        for num,i in enumerate(to_check):
-            if (check == 0):
-                if (i == 0):
-                    check = 1
-            elif (check == 1):
-                if (i == 1):
-                    start_index = num
-                    check = 2
-            elif (check == 2):
-                if (i == 0):
-                    end_index = num
-                    break
-        time_start = self.vals['fft_ready']['x'][start_index]
-        time_end = self.vals['fft_ready']['x'][end_index]
-        print(f"FFT ready goes high at {time_start} and goes low at {time_end}")
-
-        bin_slice = self.get_values_between_time(self.vals['bin']['x'], self.vals['bin']['y'], time_start, time_end)
-        real_slice = self.get_values_between_time(self.vals['ch1_val_re']['x'], self.vals['ch1_val_re']['y'], time_start, time_end)
-
-        fig, ax = plt.subplots()
-
-        #title = self.signals_of_interest["pks0"]["Title"]
-        fig.suptitle("here", fontsize = 20)
-        ax.set_ylabel("here2", fontsize=14)
-        #ax.set_yscale('log')
-        ax.set_xlabel('MHz', fontsize=14)
-        ax.ticklabel_format(style='plain', useOffset=False, axis='x')
-        ax.plot(bin_slice, real_slice)
-        plt.show()
-
-    def get_values_between_time(self, x, y, start, end):
-        check_start = True
-        for num,i in enumerate(x):
-            if (check_start == True):
-                if i >= start:
-                    start_index = num
-                    check_start = False
-            if i >= end:
-                end_index = num
-                break
-        value_slice = []
-        for i in range(start_index, end_index, 1):
-            value_slice.append(y[i])
-        return(value_slice)
-
-    def twos_comp(self, val, bits):
-        """compute the 2's complement of int value val"""
-        if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-            val = val - (1 << bits)        # compute negative value
-        return val                         # return positive value as is
-
-    def __init__(self):
-        print("Python --> Welcome to the LuSEE VCD Converter")
-        self.pp = pprint.PrettyPrinter(indent=4)
-        self.start_time = 2673450000
-        self.end_time = 2878250000
-        self.time_tick = 100000
 
 if __name__ == "__main__":
     #Start was 20644000 ns
